4ab9ac92eee34aeb1564313897972332a88b8ece
mkrana <mukulrajrana@gmail.com>
2022-07-04 14:03:01 +0530

    Add Spring boot config

---
+++ src/test/java/com/petstore/PetStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
